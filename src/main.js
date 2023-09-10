7be52d077e5a7bae6c4c125c596ccbd26016c002
Addison <S10222525@connect.np.edu.sg>
2023-09-10 16:04:51 +0800

    refactor: code structure & conditions - improved code "flow"

--- src/main.js
+++ src/main.js
@@ -1,52 +1,58 @@
-var prevTab = 0;
+var currentTab = 0;
+var prevTab = null;
 var targetTab = null;
 var log = []
 
-chrome.tabs.onActivated.addListener(function(tab, info) {
-  const files = ["functions.js"];
-
-  // Init prevTab Value
-  if (prevTab === 0) prevTab = tab.tabId;
-
-  // Log tabId : To retrieve target tab (Stop logging if PiP is Enabled)
-  if (targetTab === null) log.push(tab.tabId)
-  
-  // [ Enter PiP ] --> If video is found
-  if (prevTab != tab.tabId && targetTab == null){
-    chrome.scripting.executeScript({
-      target: { tabId: prevTab, allFrames: true },
-      world: "MAIN",
-      files,
-    }, (results) => {
-      console.log("Has Video:", results[0].result && results[0].result != null)
-      if (results[0].result && results[0].result != null){
-        console.log("Enter PiP")
-        targetTab = log[log.length - 2];
-        log = []
-      }
-    });
-  }
-
-  // [ Exit PiP ] --> If in target tab
-  if (targetTab === tab.tabId) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.tabId, allFrames: true },
-      world: "MAIN",
-      files,
-    }, (results) => {
-      if (results[0].result === "Exit"){
-        console.log("Exit PiP")
-        targetTab = null;
-      }
-    });
-  }
-
+chrome.tabs.onActivated.addListener(function(tab) {
   console.clear();
+  currentTab = tab.tabId;
+  
+  // --- [1] : Check for playing videos *(set target)  ---
+  if (targetTab === null){
+    console.log(">> Check PiP For:", currentTab)
+    chrome.scripting.executeScript({target: {tabId: currentTab}, files: ['./scripts/check-video.js']}, (results) => {
+      console.log("Has Video:", results[0].result);
+      if (results[0].result) targetTab = currentTab;
+      else {}
+    });
+  }
+
+  // --- [2] : Exit PiP *(if user is in target tab)  ---
+  if (currentTab === targetTab) {
+    console.log(">> Exit PiP")
+
+    // Execute Exit PiP
+    chrome.scripting.executeScript({target: {tabId: targetTab}, files: ['./scripts/pip.js']}, (results) => {
+      console.log("PiP:", results[0].result);
+      targetTab = null;
+    });
+
+    targetTab = null;
+  }
+
+  // --- [3] : Toggle PiP *(if there is a targetTab AND user is not in target tab)  ---
+  if (targetTab != null && currentTab != targetTab){
+
+    // [3.1] : Check if there is already a PiP vide
+    console.log(">> (CHECK) Toggle PiP")
+    chrome.scripting.executeScript({target: {tabId: targetTab}, files: ['./scripts/check-pip.js']}, (results) => {
+      console.log("PiP Exists:", results[0].result);
+
+      // [3.2] : No PiP video ; toggle PiP
+      if (!results[0].result) {
+        console.log(">> (ACTION) Toggle PiP")
+        chrome.scripting.executeScript({target: {tabId: targetTab}, files: ['./scripts/pip.js']}, (results) => {
+          console.log("PiP:", results[0].result);
+        });
+      }
+    });
+  }
+
+
   console.log("Current:", tab)
   console.log("Previous:", prevTab)
   console.log("Target:", targetTab)
-  console.log("Log:", log)
 
-  // Update prevTab Value
+  // --- [ Update ] ---
   prevTab = tab.tabId;
 });
