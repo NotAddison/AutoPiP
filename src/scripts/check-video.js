e684b6cf9f8504c6ef4965e5c190e70c677cc3a7
Addison <S10222525@connect.np.edu.sg>
2023-09-10 16:03:22 +0800

    feat: video check - check if current tab has a playing video - if so ; update target tab

---
+++ src/scripts/check-video.js
@@ -0,0 +1,17 @@
+// --- [ FUNCTION: Get Video ] --- //
+function hasPlayingVideo() {
+  const videos = Array.from(document.querySelectorAll('video'))
+    .filter(video => video.readyState != 0)
+    .filter(video => video.disablePictureInPicture == false)
+    .filter(video => video.currentTime > 0 && !video.paused && !video.ended)
+    .sort((v1, v2) => {
+      const v1Rect = v1.getClientRects()[0]||{width:0,height:0};
+      const v2Rect = v2.getClientRects()[0]||{width:0,height:0};
+      return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
+    });
+
+  if (videos.length === 0) return false;
+  return true;
+}
+
+hasPlayingVideo();
